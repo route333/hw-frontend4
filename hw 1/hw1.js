34ba3ba8cd56d00a0944cd62056d38dae873dc10
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2023-11-30 15:00:51 +0200

    hw1

---
+++ hw 1/hw1.js
@@ -0,0 +1,15 @@
+const gallery = document.querySelector(".js-gallery");
+const lightbox = document.querySelector(".js-lightbox");
+const lightboxImage = document.querySelector(".lightbox__image");
+
+gallery.addEventListener("click",(e) => {
+  if(e.target.nodeName === "IMG"){
+    const dataSource = e.target.dataset.source;
+    lightboxImage.setAttribute("src", dataSource);
+    lightbox.classList.add("is-open");
+  }
+});
+
+lightbox.addEventListener("click", function () {
+    lightbox.classList.remove("is-open");
+});
